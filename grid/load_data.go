9ef932b66700a2dc57ab532d36782585c1487486
Mark Dittmer <mark.s.dittmer@gmail.com>
2018-06-13 19:57:38 -0400

    Grid test: Save data; test out with just five runs

--- grid/load_data.go
+++ grid/load_data.go
@@ -7,6 +7,8 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
+	"strconv"
 
 	"cloud.google.com/go/datastore"
 	"github.com/web-platform-tests/results-analysis/metrics"
@@ -26,7 +28,7 @@ func init() {
 }
 
 func getRuns(ctx context.Context, client *datastore.Client) ([]*datastore.Key, []shared.TestRun) {
-	query := datastore.NewQuery("TestRun").Order("CreatedAt")
+	query := datastore.NewQuery("TestRun").Order("CreatedAt").Limit(2)
 	keys := make([]*datastore.Key, 0)
 	testRuns := make([]shared.TestRun, 0)
 	it := client.Run(ctx, query)
@@ -64,11 +66,14 @@ func main() {
 	}
 	nextRunID := 0
 	nextTestID := 0
-	testMap := make(map[string]bool)
+	testMap := make(map[string]int)
 	runSlice := make([]Run, 0)
 	testSlice := make([]Test, 0)
+	testsToRuns := make(map[string]map[string]metrics.CompleteTestStatus)
+	runsToTests := make(map[string]map[string]metrics.CompleteTestStatus)
 	_, runs := getRuns(ctx, client)
 	for _, run := range runs {
+		runID := strconv.Itoa(nextRunID)
 		runSlice = append(runSlice, Run{
 			ID:      nextRunID,
 			TestRun: run,
@@ -82,7 +87,7 @@ func main() {
 		}
 		defer resp.Body.Close()
 		if resp.StatusCode != http.StatusOK {
-			log.Printf("WARN: Non-OK HTTP status code of %s from \"%s\" for %v", resp.StatusCode, run.RawResultsURL, run)
+			log.Printf("WARN: Non-OK HTTP status code of %d from \"%s\" for %v", resp.StatusCode, run.RawResultsURL, run)
 			continue
 		}
 		data, err := ioutil.ReadAll(resp.Body)
@@ -97,28 +102,103 @@ func main() {
 		for _, res := range report.Results {
 			baseName := res.Test
 			if _, ok := testMap[baseName]; !ok {
-				testMap[baseName] = true
+				testMap[baseName] = nextTestID
 				testSlice = append(testSlice, Test{
 					ID:   nextTestID,
 					Test: res.Test,
 				})
 				nextTestID++
 			}
+			status := metrics.CompleteTestStatus{
+				Status: metrics.TestStatusFromString(res.Status),
+			}
+			testID := strconv.Itoa(testMap[baseName])
+			if _, ok := runsToTests[runID]; !ok {
+				runsToTests[runID] = make(map[string]metrics.CompleteTestStatus)
+			}
+			runsToTests[runID][testID] = status
+			if _, ok := testsToRuns[testID]; !ok {
+				testsToRuns[testID] = make(map[string]metrics.CompleteTestStatus)
+			}
+			testsToRuns[testID][runID] = status
 			for _, sub := range res.Subtests {
 				subName := baseName + ":" + sub.Name
 				if _, ok := testMap[subName]; !ok {
-					testMap[subName] = true
+					testMap[subName] = nextTestID
 					testSlice = append(testSlice, Test{
 						ID:      nextTestID,
 						Test:    res.Test,
-						SubTest: &sub.Name,
+						Subtest: sub.Name,
 					})
 					nextTestID++
 				}
+				status := metrics.CompleteTestStatus{
+					Status:    metrics.TestStatusFromString(res.Status),
+					SubStatus: metrics.SubTestStatusFromString(sub.Status),
+				}
+				runsToTests[runID][testID] = status
+				testsToRuns[testID][runID] = status
 			}
 		}
 	}
 
 	log.Printf("%d runs and %d tests", len(runSlice), len(testSlice))
 
+	{
+		runsJSON, err := json.Marshal(runSlice)
+		if err != nil {
+			log.Printf("Failed to marshal runs: %v", err)
+		} else {
+			err = ioutil.WriteFile("runs.json", runsJSON, 0644)
+			if err != nil {
+				log.Printf("Failed to save runs: %v", err)
+			}
+		}
+	}
+	runSlice = nil
+
+	{
+		testsJSON, err := json.Marshal(testSlice)
+		if err != nil {
+			log.Printf("Failed to marshal tests: %v", err)
+		} else {
+			err = ioutil.WriteFile("tests.json", testsJSON, 0644)
+			if err != nil {
+				log.Printf("Failed to save tests: %v", err)
+			}
+		}
+	}
+	testSlice = nil
+
+	{
+		os.Mkdir("by_run", 0700)
+		for runName, data := range runsToTests {
+			json, err := json.Marshal(data)
+			if err != nil {
+				log.Printf("Failed to marshal tests for run %s: %v", runName, err)
+			} else {
+				err = ioutil.WriteFile("by_run/"+runName+".json", json, 0644)
+				if err != nil {
+					log.Printf("Failed to save tests for run %s: %v", runName, err)
+				}
+			}
+		}
+	}
+	runsToTests = nil
+
+	{
+		os.Mkdir("by_test", 0700)
+		for testName, data := range testsToRuns {
+			json, err := json.Marshal(data)
+			if err != nil {
+				log.Printf("Failed to marshal runs for test %s: %v", testName, err)
+			} else {
+				err = ioutil.WriteFile("by_test/"+testName+".json", json, 0644)
+				if err != nil {
+					log.Printf("Failed to save runs for test %s: %v", testName, err)
+				}
+			}
+		}
+	}
+	testsToRuns = nil
 }
